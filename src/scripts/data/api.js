c4d61eb5194b19f9a32b7f1db3497ead58b2328a
Setianing Budi <setianingbudi2@gmail.com>
2025-06-20 21:06:50 +0700

    adding a file for server communication config

--- src/scripts/data/api.js
+++ src/scripts/data/api.js
@@ -1,10 +1,43 @@
-import CONFIG from '../config';
+import CONFIG from '../utils/config';
 
-const ENDPOINTS = {
-  ENDPOINT: `${CONFIG.BASE_URL}/your/endpoint/here`,
+const API_ENDPOINT = {
+  STORIES: `${CONFIG.BASE_URL}/stories`,
+  ADD_STORY: `${CONFIG.BASE_URL}/stories`,
 };
 
-export async function getData() {
-  const fetchResponse = await fetch(ENDPOINTS.ENDPOINT);
-  return await fetchResponse.json();
+class StoryApi {
+  // a function for getting all of the stories data
+  static async getAllStories() {
+    const response = await fetch(API_ENDPOINT.STORIES);
+    
+    if (!response.ok) {
+      throw new Error('Failed to load story');
+    }
+
+    const responseJson = await response.json();
+    return responseJson.listStory;
+  }
+
+  // a function for adding a new story
+  static async addNewStory(storyData) {
+    const formData = new FormData();
+    formData.append('photo', storyData.photo);
+    formData.append('description', storyData.description);
+    formData.append('lat', storyData.lat);
+    formData.append('lon', storyData.lon);
+
+    const response = await fetch(API_ENDPOINT.ADD_STORY, {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const responseJson = await response.json();
+      throw new Error(responseJson.message || 'Failed to add story');
+    }
+    
+    return response.json();
+  }
 }
+
+export default StoryApi;
