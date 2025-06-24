948f1e91bba3c5db8102f1a94cf9f99da1a82f42
Setianing Budi <setianingbudi2@gmail.com>
2025-06-24 08:48:00 +0700

    adding session storage utils

---
+++ src/scripts/utils/session-storage.js
@@ -0,0 +1,15 @@
+const UserSession = {
+  setUserToken(token) {
+    sessionStorage.setItem('userToken', token);
+  },
+
+  getUserToken() {
+    return sessionStorage.getItem('userToken');
+  },
+
+  removeUserToken() {
+    sessionStorage.removeItem('userToken');
+  },
+};
+
+export default UserSession;
